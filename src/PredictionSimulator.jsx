ca48a480d72ed917e6d32111727cd704582f0f93
KARTHIKKURAGANTI <urstrulykartheeek@gmail.com>
2026-03-17 00:06:33 +0530

    Implement loading state in PredictionSimulator-task3 useEffect
    Added loading state to simulate prediction delay.

--- src/PredictionSimulator.jsx
+++ src/PredictionSimulator.jsx
@@ -1,21 +1,25 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { Button } from "./Button";
 import { Card } from "./Card";
 function PredictionSimulator(){
     const [state,setState]=useState(0)
+    const [loading,setLoading] = useState(false);
 
     useEffect(() =>{
         console.log("new prediction generated...");
     },[state]);
 
     const accGenerator = () => {
-        setState(Math.floor(Math.random() * 20) + 80);
+        setLoading(true);
+        setTimeout(() => {
+            setState(Math.floor(Math.random() * 20) + 80);
+    },4000);
     }
-
 return (
     <>
     <h1>FAKE AI PREDICTOR</h1>
     <Button bname="Run Model" fun={accGenerator}/>
+    {loading && <p>Loading...</p> }
     {state>0 && (<Card title="Accuracy" value={`${state}%`}/>)}
     </>
 );
