747adcb2ace388cfc06d8cfa72136d1170fcea0b
KARTHIKKURAGANTI <urstrulykartheeek@gmail.com>
2026-03-16 23:31:06 +0530

    Implement alert on Header component load-task1 of useEffect hook
    Added useEffect to alert when the Header component loads.

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 export function Header(){
+    useEffect(() => {
+            window.alert("AI DAHBOARD LOADED SUCCESSFULLY...");
+        },[]);
     return (
         <>
         <h1>AIML DASHBOARD</h1>
