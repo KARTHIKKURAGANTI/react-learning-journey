4cbc1083bfe11b5d86208a168ed26a0c9d407016
KARTHIKKURAGANTI <urstrulykartheeek@gmail.com>
2026-03-16 23:51:53 +0530

    Implement logging for new predictions in PredictionSimulator-task2 useEffect
    Added useEffect to log when a new prediction is generated.

--- src/PredictionSimulator.jsx
+++ src/PredictionSimulator.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./Button";
 import { Card } from "./Card";
 function PredictionSimulator(){
     const [state,setState]=useState(0)
+
+    useEffect(() =>{
+        console.log("new prediction generated...");
+    },[state]);
+
     const accGenerator = () => {
         setState(Math.floor(Math.random() * 20) + 80);
     }
+
 return (
     <>
     <h1>FAKE AI PREDICTOR</h1>
